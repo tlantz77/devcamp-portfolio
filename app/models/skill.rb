279a5ec778d66b76d77838a2f332dae37754946c
tlantz77 <tlantz@gmail.com>
2021-02-16 16:12:11 -0600

    implement default image concern

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,10 @@
 class Skill < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :percent_utilized
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.badge ||= Placeholder.image_generator(height: '50', width: '50')
+  end
 end
